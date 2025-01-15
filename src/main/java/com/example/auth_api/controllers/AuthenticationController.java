81520ca3faa4f0a1ed3aa880fd1d99327e85df11
Abdulaziz aljuhani <abdulazizaljuhani2002@gmail.com>
2025-01-15 10:34:57 +0300

    add hello azoz

--- src/main/java/com/example/auth_api/controllers/AuthenticationController.java
+++ src/main/java/com/example/auth_api/controllers/AuthenticationController.java
@@ -27,6 +27,7 @@ public class AuthenticationController {
     @PostMapping("/signup")
     public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
         User registeredUser = authenticationService.signup(registerUserDto);
+        System.out.println("hello Azoz");
 
         return ResponseEntity.ok(registeredUser);
     }
